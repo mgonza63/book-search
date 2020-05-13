54ca79ad58cb8ffea01815643973368666f8d434
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-05-13 17:18:29 -0500

    added api on the server

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
-
+require('dotenv').config();
 const app = express();
-
+const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true } || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
